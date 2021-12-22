8d796add034d7af32bf851babcf2be91f1928723
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-22 09:55:17 -0600

    first implementation for Lazy loading and Suspense fallback

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,33 +1,36 @@
+import { Suspense } from "react"
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom"
 import { routes } from './Routes'
 import logo from '../logo.svg'
 
 export const Navigation = () => {
     return(
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={ logo } alt="React logo" />
-                    <ul>
+        <Suspense fallback={<span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={ logo } alt="React logo" />
+                        <ul>
+                            {
+                                routes.map((r) => (
+                                    <li key={r.to}>
+                                        <NavLink to={r.to} className={({isActive}) => isActive ? 'nav-active' : ''} >{r.name}</NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                    <Routes>
                         {
                             routes.map((r) => (
-                                <li key={r.to}>
-                                    <NavLink to={r.to} className={({isActive}) => isActive ? 'nav-active' : ''} >{r.name}</NavLink>
-                                </li>
+                                <Route key={r.path} path={r.path} element={ <r.Component/> }/>
                             ))
                         }
-                    </ul>
-                </nav>
-                <Routes>
-                    {
-                        routes.map((r) => (
-                            <Route key={r.path} path={r.path} element={ <r.Component/> }/>
-                        ))
-                    }
-                    <Route path="/*" element={ <Navigate to="/lazy1" replace/> }/>
-                </Routes>
-            </div>
+                        <Route path="/*" element={ <Navigate to="/lazy1" replace/> }/>
+                    </Routes>
+                </div>
 
-        </BrowserRouter>
+            </BrowserRouter>
+        </Suspense>
     )
 }
