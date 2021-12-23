63d8338a23ac115b007afecc5605fab694441a5a
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-23 11:48:08 -0600

    implement props interfaces for ProductCard

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,14 +1,26 @@
 import styles from '../styles/styles.module.css'
 import NoImage from '../assets/no-image.jpg'
 import { useProduct } from '../hooks/useProduct'
-export const ProductCard = () => {
+
+interface Props{
+    product: Product
+}
+interface Product{
+    id: string
+    title: string
+    img?: string
+}
+
+export const ProductCard = ({ product }: Props) => {
+
     const { counter, increaseBy } = useProduct()
+
     return(
         <div className={styles.productCard}>
-            <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe mug" />
+            <img className={styles.productImg} src={ product.img ? product.img : NoImage } alt="Coffe mug" />
             {/* <img className={styles.productImg} src={NoImage} alt="Coffe mug" /> */}
 
-            <span className={ styles.productDescription }>Coffee mug</span>
+            <span className={ styles.productDescription }>{product.title}</span>
             <div className={ styles.buttonsContainer }>
                 <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
                 <div className={styles.countLabel}> {counter} </div>
