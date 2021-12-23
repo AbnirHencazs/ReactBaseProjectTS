7af48650a85deffe0b6e845fea401852dbacdf70
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-23 14:38:46 -0600

    first successful implementation of the compound component pattern thorugh useContext

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,34 +1,51 @@
 import styles from '../styles/styles.module.css'
 import NoImage from '../assets/no-image.jpg'
 import { useProduct } from '../hooks/useProduct'
+import { createContext, ReactElement, useContext } from 'react'
 
 interface Props{
     product: Product
+    children?: ReactElement | ReactElement[]
 }
 interface Product{
     id: string
     title: string
     img?: string
 }
-
-export const ProductImage = ({ img = '' }) => {
-    return(
-        <img className={styles.productImg} src={ img ? img : NoImage } alt="Product image" />
-    )
-}
-
-export const ProductTitle = ({ title } : { title: string }) => {
-    return(
-        <span className={ styles.productDescription }>{title}</span>
-    )
-}
-
-interface ProductButtonsProps{
+interface ProductContextProps{
     counter: number
     increaseBy: (value: number) => void
+    product: Product
 }
 
-export const ProductButtons = ({counter, increaseBy}: ProductButtonsProps) => {
+const ProductContext = createContext({} as ProductContextProps)
+
+const { Provider } = ProductContext
+
+export const ProductImage = ({ img = '' }) => {
+    const { product } = useContext(ProductContext)
+    let imgToShow: string
+    if(img){
+        imgToShow = img
+    }else if(product.img){
+        imgToShow = product.img
+    }else{
+        imgToShow = NoImage
+    }
+    return(
+        <img className={styles.productImg} src={ imgToShow } alt="Product image" />
+    )
+}
+
+export const ProductTitle = ({ title } : { title?: string }) => {
+    const { product } = useContext(ProductContext)
+    return(
+        <span className={ styles.productDescription }>{title ? title : product.title}</span>
+    )
+}
+
+export const ProductButtons = () => {
+    const { counter, increaseBy } = useContext(ProductContext)
     return(
         <div className={ styles.buttonsContainer }>
             <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
@@ -38,15 +55,18 @@ export const ProductButtons = ({counter, increaseBy}: ProductButtonsProps) => {
     )
 }
 
-export const ProductCard = ({ product }: Props) => {
+export const ProductCard = ({ children, product }: Props) => {
 
     const { counter, increaseBy } = useProduct()
 
     return(
-        <div className={styles.productCard}>
-            <ProductImage />
-            <ProductTitle title={ product.title }/>
-            <ProductButtons counter={counter} increaseBy={increaseBy}/>
-        </div>
+        <Provider value={{ counter, increaseBy, product }}>
+            <div className={styles.productCard}>
+                {/* <ProductImage img={product.img}/>
+                <ProductTitle title={ product.title }/>
+                <ProductButtons counter={counter} increaseBy={increaseBy}/> */}
+                { children }
+            </div>
+        </Provider>
     )
 }
